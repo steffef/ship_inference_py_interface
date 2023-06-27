ad5ee118f03d5ade6070d41244c2a581e56f13c9
Steffen Folåsen <steffenf97@gmail.com>
2023-06-27 11:03:08 +0200

    Added namespace and parameters

--- src/intention_model_interface.cpp
+++ src/intention_model_interface.cpp
@@ -1,17 +1,23 @@
 //#include "intention_model_interface.h"
 #include <pybind11/pybind11.h>
+#include <map>
+#include "Eigen/Dense"
 #include "intention_model.h"
+#include "parameters.h"
 
 namespace py = pybind11;
 constexpr auto byref = py::return_value_policy::reference_internal;
+namespace INTENTION_INFERENCE
+{
+  PYBIND11_MODULE(intention_model, m) {
+    m.doc() = "optional module docstring";
+    py::class_<IntentionModelParameters>(m, "IntentionModelParameters");
 
-PYBIND11_MODULE(intention_model, m) {
-  m.doc() = "optional module docstring";
-
-  py::class_<Intention_Model>(m, "Intention_Model")
-    .def(py::init<std::string, const IntentionModelParameters&, int,
-         const std::map<int, Eigen::Vector4d>&,
-         const std::map<std::string, std::string>&>())
-    .def("insertObservation", &Intention_Model::insertObservation, py::call_guard<py::gil_scoped_release>())
-    ;
+    py::class_<IntentionModel>(m, "IntentionModel")
+      .def(py::init<std::string, const IntentionModelParameters&, int,
+           const std::map<int, Eigen::Vector4d>&,
+           const std::map<std::string, std::string>&>())
+      .def("insertObservation", &IntentionModel::insertObservation)
+      .def("insertObservationRelativeSituation", &IntentionModel::insertObservationRelativeSituation);
+  }
 }
