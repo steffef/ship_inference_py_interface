e423604c64a7b6bd7fe5841ad2c1d83298d0e492
Steffen Folåsen <steffenf97@gmail.com>
2023-06-27 15:45:27 +0200

    Testeing Param class. Needs constructor

--- src/intention_model_interface.cpp
+++ src/intention_model_interface.cpp
@@ -1,21 +1,21 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 #include "intention_model.h"
 #include "parameters.h"
-#include <Eigen/Dense>
 
 namespace py = pybind11;
 namespace INTENTION_INFERENCE
 {
-  PYBIND11_MODULE(intention_model_interface, m) {
+  PYBIND11_MODULE(IntentionModelInterface, m) {
     m.doc() = "optional module docstring";
     py::class_<IntentionModelParameters>(m, "IntentionModelParameters");
 
-    py::class_<IntentionModel>(m, "IntentionModel")
-      .def(py::init<std::string, const IntentionModelParameters&, int,
-           const std::map<int, Eigen::Vector4d>&,
-           const std::map<std::string, std::string>&>())
-      .def("insertObservation", &IntentionModel::insertObservation)
-      .def("insertObservationRelativeSituation",
-           &IntentionModel::insertObservationRelativeSituation);
+    // py::class_<IntentionModel>(m, "IntentionModel")
+    //   .def(py::init<std::string, const IntentionModelParameters&, int,
+    //        const std::map<int, Eigen::Vector4d>&,
+    //        const std::map<std::string, std::string>&>())
+    //   .def("insertObservation", &IntentionModel::insertObservation)
+    //   .def("insertObservationRelativeSituation",
+    //        &IntentionModel::insertObservationRelativeSituation);
   }
 }
