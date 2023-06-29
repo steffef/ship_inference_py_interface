7c009c061c6c74b5d4505a5d070e540be931ab0e
Steffen Folåsen <steffenf97@gmail.com>
2023-06-29 14:33:43 +0200

    Added the rest of the variables to parameters. Added prints to test script

--- src/intention_model_interface.cpp
+++ src/intention_model_interface.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h> //This autamtiacally maps map tupes to dicts
 #include <pybind11/eigen.h>
 #include "intention_model.h"
 #include "parameters.h"
@@ -33,6 +34,92 @@ namespace INTENTION_INFERENCE {
             .def_readwrite("minimal_accepted_by_ownship",
                            &decltype(IntentionModelParameters::ample_time_s)::minimal_accepted_by_ownship);
 
+        py::class_<decltype(IntentionModelParameters::safe_distance_m)>(m, "safe_distance_m")
+            .def(py::init<>())
+            .def_readwrite("mu",
+                           &decltype(IntentionModelParameters::safe_distance_m)::mu)
+            .def_readwrite("sigma",
+                           &decltype(IntentionModelParameters::safe_distance_m)::sigma)
+            .def_readwrite("max",
+                           &decltype(IntentionModelParameters::safe_distance_m)::max)
+            .def_readwrite("n_bins",
+                           &decltype(IntentionModelParameters::safe_distance_m)::n_bins);
+
+        py::class_<decltype(IntentionModelParameters::risk_distance_m)>(m, "risk_distance_m")
+            .def(py::init<>())
+            .def_readwrite("mu",
+                           &decltype(IntentionModelParameters::risk_distance_m)::mu)
+            .def_readwrite("sigma",
+                           &decltype(IntentionModelParameters::risk_distance_m)::sigma)
+            .def_readwrite("max",
+                           &decltype(IntentionModelParameters::risk_distance_m)::max)
+            .def_readwrite("n_bins",
+                           &decltype(IntentionModelParameters::risk_distance_m)::n_bins);
+
+        py::class_<decltype(IntentionModelParameters::risk_distance_front_m)>(m, "risk_distance_front_m")
+            .def(py::init<>())
+            .def_readwrite("mu",
+                           &decltype(IntentionModelParameters::risk_distance_front_m)::mu)
+            .def_readwrite("sigma",
+                           &decltype(IntentionModelParameters::risk_distance_front_m)::sigma)
+            .def_readwrite("max",
+                           &decltype(IntentionModelParameters::risk_distance_front_m)::max)
+            .def_readwrite("n_bins",
+                           &decltype(IntentionModelParameters::risk_distance_front_m)::n_bins);
+
+        py::class_<decltype(IntentionModelParameters::safe_distance_midpoint_m)>(m, "safe_distance_midpoint_m")
+            .def(py::init<>())
+            .def_readwrite("mu",
+                           &decltype(IntentionModelParameters::safe_distance_midpoint_m)::mu)
+            .def_readwrite("sigma",
+                           &decltype(IntentionModelParameters::safe_distance_midpoint_m)::sigma)
+            .def_readwrite("max",
+                           &decltype(IntentionModelParameters::safe_distance_midpoint_m)::max)
+            .def_readwrite("n_bins",
+                           &decltype(IntentionModelParameters::safe_distance_midpoint_m)::n_bins);
+
+        py::class_<decltype(IntentionModelParameters::safe_distance_front_m)>(m, "safe_distance_front_m")
+            .def(py::init<>())
+            .def_readwrite("mu",
+                           &decltype(IntentionModelParameters::safe_distance_front_m)::mu)
+            .def_readwrite("sigma",
+                           &decltype(IntentionModelParameters::safe_distance_front_m)::sigma)
+            .def_readwrite("max",
+                           &decltype(IntentionModelParameters::safe_distance_front_m)::max)
+            .def_readwrite("n_bins",
+                           &decltype(IntentionModelParameters::safe_distance_front_m)::n_bins);
+
+        py::class_<decltype(IntentionModelParameters::change_in_course_rad)>(m, "change_in_course_rad")
+            .def(py::init<>())
+            .def_readwrite("minimal_change",
+                           &decltype(IntentionModelParameters::change_in_course_rad)::minimal_change);
+
+        py::class_<decltype(IntentionModelParameters::change_in_speed_m_s)>(m, "change_in_speed_m_s")
+            .def(py::init<>())
+            .def_readwrite("minimal_change",
+                           &decltype(IntentionModelParameters::change_in_speed_m_s)::minimal_change);
+
+        py::class_<decltype(IntentionModelParameters::colregs_situation_borders_rad)>(m, "colregs_situation_borders_rad")
+            .def(py::init<>())
+            .def_readwrite("HO_uncertainty_start",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::HO_uncertainty_start)
+            .def_readwrite("HO_start",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::HO_start)
+            .def_readwrite("HO_stop",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::HO_stop)
+            .def_readwrite("HO_uncertainty_stop",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::HO_uncertainty_stop)
+            .def_readwrite("OT_uncertainty_start",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::OT_uncertainty_start)
+            .def_readwrite("OT_start",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::OT_start)
+            .def_readwrite("OT_stop",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::OT_stop)
+            .def_readwrite("OT_uncertainty_stop",
+                           &decltype(IntentionModelParameters::colregs_situation_borders_rad)::OT_uncertainty_stop);
+
+        py::bind_map<std::map<std::string, double>>(m, "StringDoubleMap");
+
         py::class_<IntentionModelParameters>(m, "IntentionModelParameters")
             .def(py::init<>())
             .def_readwrite("number_of_network_evaluation_samples",
@@ -48,67 +135,33 @@ namespace INTENTION_INFERENCE {
             .def_readwrite("expanding_dbn",
                            &IntentionModelParameters::expanding_dbn)
             .def_readwrite("ample_time_s",
-                           &IntentionModelParameters::ample_time_s);
- //         struct{
-//             double mu;
-//             double sigma;
-//             double max;
-//             unsigned n_bins;
-//             double minimal_accepted_by_ownship;
-//         }ample_time_s;
-//         struct{
-//             double mu;
-//         double sigma;
-//         double max;
-//         unsigned n_bins;
-//     }safe_distance_m;
-//     struct{
-//         double mu;
-//         double sigma;
-//         double max;
-//         unsigned n_bins;
-//     }
-//     risk_distance_m;
-//     struct{
-//         double mu;
-//         double sigma;
-//         double max;
-//         unsigned n_bins;
-//     }risk_distance_front_m;
-//     struct{
-//         double mu;
-//         double sigma;
-//         double max;
-//         unsigned n_bins;
-//     }safe_distance_midpoint_m;
-//     struct{
-//         double mu;
-//         double sigma;
-//         double max;
-//         unsigned n_bins;
-//     }safe_distance_front_m;
-//     struct{
-//         double minimal_change;
-//     }change_in_course_rad;
-//     struct{
-//         double minimal_change;
-//     }change_in_speed_m_s;
-//     struct{
-//         double HO_uncertainty_start;
-//         double HO_start;
-//         double HO_stop;
-//         double HO_uncertainty_stop;
-//         double OT_uncertainty_start;
-//         double OT_start;
-//         double OT_stop;
-//         double OT_uncertainty_stop;
-//     }colregs_situation_borders_rad;
-//     double ignoring_safety_probability;
-//     double colregs_compliance_probability;
-//     double good_seamanship_probability;
-//     double unmodeled_behaviour;
-//     std::map<std::string, double> priority_probability;
-// }
+                           &IntentionModelParameters::ample_time_s)
+            .def_readwrite("safe_distance_m",
+                           &IntentionModelParameters::safe_distance_m)
+            .def_readwrite("risk_distance_m",
+                           &IntentionModelParameters::risk_distance_m)
+            .def_readwrite("risk_distance_front_m",
+                           &IntentionModelParameters::risk_distance_front_m)
+            .def_readwrite("safe_distance_midpoint_m",
+                           &IntentionModelParameters::safe_distance_midpoint_m)
+            .def_readwrite("safe_distance_front_m",
+                           &IntentionModelParameters::safe_distance_front_m)
+            .def_readwrite("change_in_course_rad",
+                           &IntentionModelParameters::change_in_course_rad)
+            .def_readwrite("change_in_speed_m_s",
+                           &IntentionModelParameters::change_in_speed_m_s)
+            .def_readwrite("colregs_situation_borders_rad",
+                           &IntentionModelParameters::colregs_situation_borders_rad)
+            .def_readwrite("ignoring_safety_probability",
+                           &IntentionModelParameters::ignoring_safety_probability)
+            .def_readwrite("colregs_compliance_probability",
+                           &IntentionModelParameters::colregs_compliance_probability)
+            .def_readwrite("good_seamanship_probability",
+                           &IntentionModelParameters::good_seamanship_probability)
+            .def_readwrite("unmodeled_behaviour",
+                           &IntentionModelParameters::unmodeled_behaviour)
+            .def_readwrite("priority_probability",
+                           &IntentionModelParameters::priority_probability);
 
     // py::class_<IntentionModel>(m, "IntentionModel")
     //       .def(py::init<std::string, const IntentionModelParameters&, int,
---
+++ tests/test_parameters_interface.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("build/")
+import IntentionModelInterface as im
+
+def printModelParameters(param):
+    print("number_of_network_evaluation_samples", param.number_of_network_evaluation_samples)
+    print("max_number_of_obstacles", param.max_number_of_obstacles)
+    print("time_into_trajectory", param.time_into_trajectory)
+    print("starting_distance", param.starting_distance)
+    print("starting_cpa_distance", param.starting_cpa_distance)
+    print("expanding_dbn.min_time_s", param.expanding_dbn.min_time_s)
+    print("expanding_dbn.max_time_s", param.expanding_dbn.max_time_s)
+    print("expanding_dbn.min_course_change_rad", param.expanding_dbn.min_course_change_rad)
+    print("expanding_dbn.min_speed_change_m_s", param.expanding_dbn.min_speed_change_m_s)
+    print("ample_time_s.mu", param.ample_time_s.mu)
+    print("ample_time_s.sigma", param.ample_time_s.sigma)
+    print("ample_time_s.max", param.ample_time_s.max)
+    print("ample_time_s.n_bins", param.ample_time_s.n_bins) # this value must match the bayesian network
+    print("ample_time_s.minimal_accepted_by_ownship", param.ample_time_s.minimal_accepted_by_ownship)
+    print("safe_distance_m.mu", param.safe_distance_m.mu)
+    print("safe_distance_m.sigma", param.safe_distance_m.sigma)
+    print("safe_distance_m.max", param.safe_distance_m.max)
+    print("safe_distance_m.n_bins", param.safe_distance_m.n_bins)
+    print("risk_distance_m.mu", param.risk_distance_m.mu)
+    print("risk_distance_m.sigma", param.risk_distance_m.sigma)
+    print("risk_distance_m.max", param.risk_distance_m.max)
+    print("risk_distance_m.n_bins", param.risk_distance_m.n_bins) # this value must match the bayesian network
+    print("risk_distance_front_m.mu", param.risk_distance_front_m.mu)
+    print("risk_distance_front_m.sigma", param.risk_distance_front_m.sigma)
+    print("risk_distance_front_m.max", param.risk_distance_front_m.max)
+    print("risk_distance_front_m.n_bins", param.risk_distance_front_m.n_bins)
+    print("safe_distance_midpoint_m.mu", param.safe_distance_midpoint_m.mu)
+    print("safe_distance_midpoint_m.sigma", param.safe_distance_midpoint_m.sigma)
+    print("safe_distance_midpoint_m.max", param.safe_distance_midpoint_m.max)
+    print("safe_distance_midpoint_m.n_bins", param.safe_distance_midpoint_m.n_bins) # this value must match the bayesian network
+    print("safe_distance_front_m.mu", param.safe_distance_front_m.mu)
+    print("safe_distance_front_m.sigma", param.safe_distance_front_m.sigma)
+    print("safe_distance_front_m.max", param.safe_distance_front_m.max)
+    print("safe_distance_front_m.n_bins", param.safe_distance_front_m.n_bins) # this value must match the bayesian network
+    print("change_in_course_rad.minimal_change", param.change_in_course_rad.minimal_change)
+    print("change_in_speed_m_s.minimal_change", param.change_in_speed_m_s.minimal_change)
+    print("colregs_situation_borders_rad.HO_uncertainty_start", param.colregs_situation_borders_rad.HO_uncertainty_start)
+    print("colregs_situation_borders_rad.HO_start", param.colregs_situation_borders_rad.HO_start)
+    print("colregs_situation_borders_rad.HO_stop", param.colregs_situation_borders_rad.HO_stop)
+    print("colregs_situation_borders_rad.HO_uncertainty_stop", param.colregs_situation_borders_rad.HO_uncertainty_stop)
+    print("colregs_situation_borders_rad.OT_uncertainty_start", param.colregs_situation_borders_rad.OT_uncertainty_start)
+    print("colregs_situation_borders_rad.OT_start", param.colregs_situation_borders_rad.OT_start)
+    print("colregs_situation_borders_rad.OT_stop", param.colregs_situation_borders_rad.OT_stop)
+    print("colregs_situation_borders_rad.OT_uncertainty_stop", param.colregs_situation_borders_rad.OT_uncertainty_stop)
+    print("ignoring_safety_probability", param.ignoring_safety_probability)
+    print("colregs_compliance_probability", param.colregs_compliance_probability)
+    print("good_seamanship_probability", param.good_seamanship_probability)
+    print("unmodeled_behaviour", param.unmodeled_behaviour)
+    print("priority_probability lower", param.priority_probability["lower"])
+    print("priority_probability similar", param.priority_probability["similar"])
+    print("priority_probability higher", param.priority_probability["higher"])
+
+def setModelParameters(num_ships):
+    param = im.IntentionModelParameters()
+    param.number_of_network_evaluation_samples = 100000
+    param.max_number_of_obstacles = num_ships-1 #must be set to num_ships-1 or else segmantation fault
+    param.time_into_trajectory = 10
+    param.starting_distance = 10000
+    param.starting_cpa_distance = 15000
+    param.expanding_dbn.min_time_s = 10
+    param.expanding_dbn.max_time_s = 1200
+    param.expanding_dbn.min_course_change_rad = 0.18
+    param.expanding_dbn.min_speed_change_m_s = 2
+    param.ample_time_s.mu = 200
+    param.ample_time_s.sigma = 100
+    param.ample_time_s.max = 1000
+    param.ample_time_s.n_bins = 30 # this value must match the bayesian network
+    param.ample_time_s.minimal_accepted_by_ownship = 20
+    param.safe_distance_m.mu = 200
+    param.safe_distance_m.sigma = 30
+    param.safe_distance_m.max = 800
+    param.safe_distance_m.n_bins = 30
+    param.risk_distance_m.mu = 1800
+    param.risk_distance_m.sigma = 500
+    param.risk_distance_m.max = 2500
+    param.risk_distance_m.n_bins = 30 # this value must match the bayesian network
+    param.risk_distance_front_m.mu = 1900
+    param.risk_distance_front_m.sigma = 500
+    param.risk_distance_front_m.max = 2000
+    param.risk_distance_front_m.n_bins = 30
+    param.safe_distance_midpoint_m.mu = 600
+    param.safe_distance_midpoint_m.sigma = 20
+    param.safe_distance_midpoint_m.max = 2500
+    param.safe_distance_midpoint_m.n_bins = 30 # this value must match the bayesian network
+    param.safe_distance_front_m.mu = 100
+    param.safe_distance_front_m.sigma = 50
+    param.safe_distance_front_m.max = 1000
+    param.safe_distance_front_m.n_bins = 30 # this value must match the bayesian network
+    param.change_in_course_rad.minimal_change = 0.18
+    param.change_in_speed_m_s.minimal_change = 2
+    param.colregs_situation_borders_rad.HO_uncertainty_start = 2.79
+    param.colregs_situation_borders_rad.HO_start = 2.96
+    param.colregs_situation_borders_rad.HO_stop = -2.96
+    param.colregs_situation_borders_rad.HO_uncertainty_stop = -2.79
+    param.colregs_situation_borders_rad.OT_uncertainty_start = 1.74
+    param.colregs_situation_borders_rad.OT_start = 2.18
+    param.colregs_situation_borders_rad.OT_stop = -2.18
+    param.colregs_situation_borders_rad.OT_uncertainty_stop = -1.74
+    param.ignoring_safety_probability = 0
+    param.colregs_compliance_probability = 0.97
+    param.good_seamanship_probability = 0.99
+    param.unmodeled_behaviour = 0.001
+    param.priority_probability["lower"] = 0.05
+    param.priority_probability["similar"] = 0.90
+    param.priority_probability["higher"] = 0.05
+    return param
+
+if __name__ == "__main__":
+    num_ships = 2
+    filename = "new_1_Case - 09-17-2018, 18-24-32 - 0URFX-60-sec.csv"
+    intentionModelFilename = "intention_model_with_risk_of_collision_no_startpoint_3.xdsl"
+
+    parameters = setModelParameters(num_ships)
+    printModelParameters(parameters)
+    print("done")
